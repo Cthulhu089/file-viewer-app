ec24e9451097990a6b3466909c3da1a3f1a184fb
Marco Polo Vazquez Gutierrez <marco.vazquez@agilethought.com>
2021-08-26 11:34:49 -0500

    Prevent right click and selection

--- src/views/FileViewer/index.tsx
+++ src/views/FileViewer/index.tsx
@@ -1,15 +1,20 @@
-import { useMemo } from "react";
+import { useEffect, useMemo } from "react";
 import styled from "styled-components/macro";
 import DocViewer, {
   PDFRenderer,
   TXTRenderer,
   JPGRenderer,
   MSDocRenderer,
+  PNGRenderer,
 } from "react-doc-viewer";
 
 const StyledDocViewer = styled(DocViewer)`
   #pdf-download {
     display: none;
+  }
+
+  #proxy-renderer {
+    user-select: none;
   }
 `;
 
@@ -28,18 +33,31 @@ const FileViewerView = () => {
       {
         uri: "./image1.jpg",
       },
+      {
+        uri: "./index.png",
+      },
     ],
     []
   );
 
+  useEffect(() => {
+    const docContainer = document.getElementById("proxy-renderer");
+    if (docContainer) {
+      docContainer.addEventListener("contextmenu", (e) => e.preventDefault());
+    }
+  }, []);
+
   return (
     <div className="App">
       <StyledDocViewer
-        pluginRenderers={[PDFRenderer, TXTRenderer, JPGRenderer, MSDocRenderer]}
+        pluginRenderers={[
+          PDFRenderer,
+          TXTRenderer,
+          JPGRenderer,
+          MSDocRenderer,
+          PNGRenderer,
+        ]}
         documents={docs}
-        config={{
-          header: {},
-        }}
       />
       <header className="App-header">
         <p>
